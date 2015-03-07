9dd5167a261f0faba4857db5aa8f96bdc0db7254
Bryan Wyatt <brwyatt@gmail.com>
2015-03-07 15:30:46 -0800

    Add first version of UserData script.

---
+++ UserData.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+maintainer=JungleCatSoftware
+repo=AWS-Toolkit
+release=0.0.1
+targetdir=/opt/${maintainer}
+mkdir -p ${targetdir} &&\
+curl -L https://github.com/${maintainer}/${repo}/archive/${release}.tar.gz |\
+tar -xz -C ${targetdir} &&\
+mv ${targetdir}/${repo}-${release} ${targetdir}/${repo}
+${targetdir}/${repo}/Configure.sh
