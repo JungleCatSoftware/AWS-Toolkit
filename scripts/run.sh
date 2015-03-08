aa41f7f986f26228e46fd94e5f4fa7d6128e6b54
Bryan Wyatt <brwyatt@gmail.com>
2015-03-07 16:56:22 -0800

    Add discovery of script dirs and add to PATH

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "This was run at $(date)" > /FLAG.txt
+scriptroot=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+distro=$(lsb_release -i | sed -r 's/^Distributor ID:\W*(.*)$/\L\1/')
+release=$(lsb_release -c | sed -r 's/^Codename:\W*(.*)$/\L\1/')
+
+# Add global scripts directory
+globalscripts="${scriptroot}/global"
+if [ -d "${globalscripts}" ]; then
+  PATH="${globalscripts}:${PATH}"
+fi
+
+# Add distro-specific scripts directory
+distroscripts="${scriptroot}/${distro}/all"
+if [ -d "${distroscripts}" ]; then
+  PATH="${distroscripts}:${PATH}"
+fi
+
+# Add release-specific scripts directory
+releasescripts="${scriptroot}/${distro}/${release}"
+if [ -d "${releasescripts}" ]; then
+  PATH="${releasescripts}:${PATH}"
+fi
+
