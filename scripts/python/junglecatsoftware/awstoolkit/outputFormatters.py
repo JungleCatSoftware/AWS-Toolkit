c6712882d3dc84cc34d9daefd2a155de6edec8b7
Bryan Wyatt <brwyatt@junglecatsoftware.com>
2015-06-06 15:07:01 -0700

    Update the header print functions to use format()
    - Makes changes to printBigHeader() to make it more compact and easier to read.
    - Changes printStartDateStamp() to use format() on the datetime object

--- scripts/python/junglecatsoftware/awstoolkit/outputFormatters.py
+++ scripts/python/junglecatsoftware/awstoolkit/outputFormatters.py
@@ -1,39 +1,25 @@
 import datetime
 
 def printBigHeader( author = 'Jungle Cat Software', package = 'AWS-Toolkit', function = 'Output Formatter' ):
-  lineOne = author + ' | ' + package
-  lineOneLen = len(lineOne)
-  lineTwoLen = len(function)
+  firstLine = author + ' | ' + package
+  firstLineLen = len( firstLine )
+  secondLine = function
+  secondLineLen = len( secondLine )
 
-  biggestLen = lineOneLen if lineOneLen >= lineTwoLen else lineTwoLen
+  longestLineLen = firstLineLen if firstLineLen >= secondLineLen else secondLineLen
+  linePadLen = longestLineLen + 2
+  headerLineLen = linePadLen + 2
 
-  # Line 1
-  print('*' * (biggestLen+4))
-
-  def printLine( line = '' ):
-    print('* ', end = '')
-    spaceNeeded = (biggestLen - len(line))
-    lSpaceNeeded = (spaceNeeded // 2)
-    print(' ' * lSpaceNeeded, end = '')
-    print(line, end = '')
-    rSpaceNeeded = (spaceNeeded - lSpaceNeeded)
-    print(' ' * rSpaceNeeded, end = '')
-    print(' *')
-
-  # Line 2
-  printLine(lineOne)
-
-  # Line 3
-  printLine(function)
-
-  # Line 4
-  print('*' * (biggestLen+4))
+  print( ( '{0:*^' + str( headerLineLen ) + '}' ).format( '' ) )
+  print( ( '*{0: ^' + str( linePadLen ) + '}*' ).format( firstLine ) )
+  print( ( '*{0: ^' + str( linePadLen ) + '}*' ).format( secondLine ) )
+  print( ( '{0:*^' + str( headerLineLen ) + '}' ).format( '' ) )
 
 def printSmallHeader( function = 'Output Formatter' ):
   print('\n==' + function + '==')
 
 def printStartDateStamp():
-  print('Begin at ' + datetime.datetime.now().__str__())
+  print( 'Begin at {0:%Y-%m-d %H:%M:%S.%f}'.format( datetime.datetime.now() ) )
 
 def printIndented(text,indentLevel=1):
   for line in text.splitlines():
