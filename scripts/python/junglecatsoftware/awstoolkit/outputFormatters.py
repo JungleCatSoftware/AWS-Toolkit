e19e4c345ae7bc643731113c98903286cda6e017
Bryan Wyatt <brwyatt@junglecatsoftware.com>
2015-06-07 21:35:54 -0700

    Fix pylint errors
    This fixes indentation and space errors pointed out from pylint
    
    - Increase Two-space indents to four-spaces.
    - Remove spaces inside parenthesis.
    - Remove extra spaces around the = in parameter assignments.
    - Add __init__.py.

--- scripts/python/junglecatsoftware/awstoolkit/outputFormatters.py
+++ scripts/python/junglecatsoftware/awstoolkit/outputFormatters.py
@@ -1,30 +1,31 @@
 import datetime
 
-def printBigHeader( author = 'Jungle Cat Software', package = 'AWS-Toolkit', function = 'Output Formatter' ):
-  firstLine = author + ' | ' + package
-  firstLineLen = len( firstLine )
-  secondLine = function
-  secondLineLen = len( secondLine )
+def printBigHeader(author='Jungle Cat Software', package='AWS-Toolkit',
+                   function='Output Formatter'):
+    firstLine = author + ' | ' + package
+    firstLineLen = len(firstLine)
+    secondLine = function
+    secondLineLen = len(secondLine)
 
-  longestLineLen = firstLineLen if firstLineLen >= secondLineLen else secondLineLen
-  linePadLen = longestLineLen + 2
-  headerLineLen = linePadLen + 2
+    longestLineLen = firstLineLen if firstLineLen >= secondLineLen else secondLineLen
+    linePadLen = longestLineLen + 2
+    headerLineLen = linePadLen + 2
 
-  print('*' * headerLineLen)
-  formatStr = '*{{0: ^{}}}*'.format(linePadLen)
-  print(formatStr.format(firstLine))
-  print(formatStr.format(secondLine))
-  print('*' * headerLineLen)
+    print('*' * headerLineLen)
+    formatStr = '*{{0: ^{}}}*'.format(linePadLen)
+    print(formatStr.format(firstLine))
+    print(formatStr.format(secondLine))
+    print('*' * headerLineLen)
 
-def printSmallHeader( function = 'Output Formatter' ):
-  print('\n=={}=='.format(function))
+def printSmallHeader(function='Output Formatter'):
+    print('\n=={}=='.format(function))
 
 def printStartDateStamp():
-  print( 'Begin at {0:%Y-%m-%d %H:%M:%S.%f}'.format( datetime.datetime.now() ) )
+    print('Begin at {0:%Y-%m-%d %H:%M:%S.%f}'.format(datetime.datetime.now()))
 
-def printIndented( text, indentLevel = 1 ):
-  for line in text.splitlines():
-    print( ( '\t' * indentLevel ) + line )
+def printIndented(text, indentLevel=1):
+    for line in text.splitlines():
+        print(('\t' * indentLevel) + line)
 
 if __name__ == "__main__":
     printBigHeader()
